31a860b685a4a0e5b5f2bba79ceb4fb3557ed431
LaCasetaBBQ <la.caseta.bbq@gmail.com>
2026-06-21 19:45:22 +0200

    Using v8

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,4 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({
-  site: 'https://lacasetabbq.github.io',
-  base: '/lacasetabbq.github.io',
-});
+export default defineConfig({});
